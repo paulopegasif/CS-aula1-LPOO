a5057be954022a50981a3d048ce2b80452934770
Paulo Henrique Silva <paulosilva.pf157@academico.ifsul.edu.br>
2022-04-18 10:52:19 -0300

    -Adicionando novas classes -Criando uma enum (tipo de classe especial para constantes)

---
+++ src/br/edu/ifsul/cc/lpoo/cs/model/Municao.java
@@ -0,0 +1,24 @@
+package br.edu.ifsul.cc.lpoo.cs.model;
+
+public class Municao extends Artefato {
+    private Integer id;
+    private String nome;
+
+    public Municao(){
+    }
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ src/br/edu/ifsul/cc/lpoo/cs/model/Tipo.java
@@ -0,0 +1,4 @@
+package br.edu.ifsul.cc.lpoo.cs.model;
+
+public enum Tipo {
+}
